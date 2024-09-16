94fae560a21438532d5521084bf49f09dfdb916c
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-09-16 11:56:33 +0530

    feat: hide/show navbar when scroll up/down

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+var lastScrollTop = 0;
+navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function () {
+    var scrollTop = document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = '-80px';
+    } else {
+        navbar.style.top = '0';
+    }
+    lastScrollTop = scrollTop;
+});
+
 function myFunction(x) {
     x.classList.toggle('change');
 }
